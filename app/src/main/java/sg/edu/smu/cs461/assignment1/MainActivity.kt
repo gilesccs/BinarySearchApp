3227ade6a16078dbae1b3eac38266a81e2225652
Alwyn-Ong <52643926+Alwyn-Ong@users.noreply.github.com>
2021-01-25 19:23:18 +0800

    Integrated ui with function

--- app/src/main/java/sg/edu/smu/cs461/assignment1/MainActivity.kt
+++ app/src/main/java/sg/edu/smu/cs461/assignment1/MainActivity.kt
@@ -9,7 +9,7 @@ import android.text.style.UnderlineSpan
 import android.widget.TextView
 import android.view.View
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import java.util.*
@@ -29,32 +29,33 @@ class MainActivity : AppCompatActivity() {
         val output = findViewById<TextView>(R.id.title);
         output.text = spannableString;
 
+        generateRandomNumber()
+
     }
 
-    private fun generateRandomNumbers(){
+    private fun generateRandomNumber(){
         // Getting the random number
         var r = Random()
         // Assigning it to the var
-        randomNumber = r.nextInt(100)
+        randomNumber = r.nextInt(99) + 1
     }
 
-    private fun checkUserInput(context: Context){
+    fun checkUserInput(view: View){
         // Gets value of user input
-//        val userInput = findViewById<TextView>(R.id.userInput)
-        val userInput = 50
-//        var outputMessage = "Your number is: "
-        var outputMessage = ""
+        val userInputView = findViewById<TextView>(R.id.inputText)
+        val userInputString = userInputView.text.toString()
+        val userInputInt = Integer.parseInt(userInputString)
 
-        // To get the textview for output
-//        val output = findViewById<TextView>(R.id.output)
+        // To get the imageview for output
+        var imageView = findViewById<ImageView>(R.id.imageView)
 
-        if (userInput < randomNumber) {
-            // Changing text field data
-            outputMessage = "HIGHER"
-        } else if (userInput > randomNumber) {
-            outputMessage = "LOWER"
+        if (userInputInt < randomNumber) {
+            // Changing image view data
+            imageView.setImageResource(R.drawable.higher)
+        } else if (userInputInt > randomNumber) {
+            imageView.setImageResource(R.drawable.lower)
         } else {
-            outputMessage = "CORRECT"
+            imageView.setImageResource(R.drawable.correct)
         }
 
         // Setting output to new message
@@ -62,29 +63,55 @@ class MainActivity : AppCompatActivity() {
 
         attempts++
         // Setting attempts to new attempts
-//        val attemptsMessage = findViewById<TextView>(R.id.attemptsMessage)
-//        attemptsMessage.text = "#" + attempts.toString()
+        val attemptsMessage = findViewById<TextView>(R.id.attempt)
+        attemptsMessage.text = "Attempt # " + attempts.toString()
 
         // Stops if game is still on
-        if (userInput != randomNumber) {
+        if (userInputInt != randomNumber) {
             return
         }
 
         // Disable enter button
-//        Button b = findViewById<Button>(R.id.button)
-//        markButtonDisable(b)
+        val enterButton = findViewById<Button>(R.id.enterBtn)
+        markButtonDisable(enterButton, false)
         // Send toast
-        context.toast("Congrats! Press reset to start a new game.")
-
+//        context.toast("Congrats! Press reset to start a new game.")
+        Toast.makeText(applicationContext, "Congrats! Press New Game to restart the game.",Toast.LENGTH_LONG).show()
+        // Change button text
+        val resetButton = findViewById<Button>(R.id.resetBtn)
+        resetButton.text = "New Game"
     }
 
-    private fun markButtonDisable(button: Button) {
-        button?.isEnabled = false
-        button?.setTextColor(ContextCompat.getColor(textView.context, R.color.white))
-        button?.setBackgroundColor(ContextCompat.getColor(textView.context, R.color.greyish))
+    private fun markButtonDisable(button: Button, enable: Boolean) {
+        button?.isEnabled = enable
+//        button?.setTextColor(ContextCompat.getColor(textView.context, R.color.white))
+//        button?.setBackgroundColor(ContextCompat.getColor(textView.context, R.color.greyish))
     }
 
     fun Context.toast(message: CharSequence) =
             Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 
+    fun newGame(view: View) {
+
+        // Disable enter button
+        val enterButton = findViewById<Button>(R.id.enterBtn)
+        markButtonDisable(enterButton, true)
+
+        attempts = 0
+        var imageView = findViewById<ImageView>(R.id.imageView)
+        imageView.setImageResource(R.drawable.welcome)
+
+        val attemptsMessage = findViewById<TextView>(R.id.attempt)
+        attemptsMessage.text = "Welcome"
+
+        generateRandomNumber()
+
+        // Change button text
+        val resetButton = findViewById<Button>(R.id.resetBtn)
+        resetButton.text = "Restart"
+
+        // Change input text
+        val inputText = findViewById<TextView>(R.id.inputText)
+        inputText.text = ""
+    }
 }
